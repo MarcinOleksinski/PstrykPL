968c38b28e920edb395044fc86e1721cf37ad0bb
Marcin Oleksiński <marcin@unich.pl>
2025-05-22 13:30:04 +0200

    changes currecy

--- custom_components/pstryk_pl/sensor.py
+++ custom_components/pstryk_pl/sensor.py
@@ -1,6 +1,6 @@
 """Sensory Pstryk.pl z cenami energii."""
 from homeassistant.components.sensor import SensorEntity
-from homeassistant.const import CURRENCY_ZLOTY
+#from homeassistant.const import CURRENCY_ZLOTY
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from .const import DOMAIN
 
@@ -21,7 +21,7 @@ class PstrykPriceSensor(CoordinatorEntity, SensorEntity):
         super().__init__(coordinator)
         self._attr_unique_id = f"pstryk_price_{unique_id}"
         self._attr_name = f"Pstryk Cena energii ({frame.get('start', '')[-8:-3]})"
-        self._attr_native_unit_of_measurement = CURRENCY_ZLOTY
+        self._attr_native_unit_of_measurement = "PLN"
         self._attr_device_class = "monetary"
         self._attr_state_class = "measurement"
         self.frame = frame
