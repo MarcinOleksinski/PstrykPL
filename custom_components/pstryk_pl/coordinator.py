e3bd7b70bdd8f54c70f2a07fa686e2976d2a5074
Marcin Oleksiński <marcin@unich.pl>
2025-05-23 11:43:52 +0200

    coordinator: add support for multiple devices

--- custom_components/pstryk_pl/coordinator.py
+++ custom_components/pstryk_pl/coordinator.py
@@ -7,13 +7,13 @@ from .const import DOMAIN
 _LOGGER = logging.getLogger(__name__)
 
 
-class PstrykEnergyCostCoordinator(DataUpdateCoordinator):
+class PstrykPricingCoordinator(DataUpdateCoordinator):
     def __init__(self, hass, api: PstrykApi, resolution: str, days: int):
         super().__init__(
             hass,
             _LOGGER,
-            name="Pstryk Energy Cost Coordinator",
-            update_interval=timedelta(minutes=15),
+            name="Pstryk Pricing Coordinator",
+            update_interval=timedelta(minutes=10),
         )
         self.api = api
         self.resolution = resolution
@@ -24,6 +24,6 @@ class PstrykEnergyCostCoordinator(DataUpdateCoordinator):
             now = datetime.now(timezone.utc)
             start = now.replace(hour=0, minute=0, second=0, microsecond=0)
             end = start + timedelta(days=self.days)
-            return await self.api.get_energy_cost(start, end, resolution=self.resolution)
+            return await self.api.get_pricing(resolution=self.resolution, window_start=start, window_end=end)
         except Exception as err:
-            raise UpdateFailed(f"Error fetching energy cost data: {err}")
+            raise UpdateFailed(f"Error fetching pricing: {err}")
