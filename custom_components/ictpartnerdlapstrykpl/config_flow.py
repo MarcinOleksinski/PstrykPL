0a5a5a432691bd235c22c765a3626501fae0df95
Marcin Oleksiński <marcin@unich.pl>
2025-05-26 00:27:19 +0200

    Poprawki do wersji 1.0.1

--- custom_components/ictpartnerdlapstrykpl/config_flow.py
+++ custom_components/ictpartnerdlapstrykpl/config_flow.py
@@ -1,3 +1,23 @@
+class ICTPartnerDlaPstrykOptionsFlowHandler(config_entries.OptionsFlow):
+    def __init__(self, config_entry):
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        errors = {}
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+
+        # Use current config as defaults
+        data = self.config_entry.data
+        options = self.config_entry.options
+        schema = vol.Schema({
+            vol.Required(CONF_API_KEY, default=data.get(CONF_API_KEY, "")): str,
+            vol.Optional(CONF_METERS, default=data.get(CONF_METERS, "main")): str,
+            vol.Optional(CONF_TIMEZONE, default=data.get(CONF_TIMEZONE, "Europe/Warsaw")): str,
+            vol.Optional(CONF_ALERT_PRICE, default=options.get(CONF_ALERT_PRICE, data.get(CONF_ALERT_PRICE, 1.0))): vol.Coerce(float),
+            vol.Optional(CONF_ALERT_USAGE, default=options.get(CONF_ALERT_USAGE, data.get(CONF_ALERT_USAGE, 10.0))): vol.Coerce(float),
+        })
+        return self.async_show_form(step_id="init", data_schema=schema, errors=errors)
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.const import CONF_API_KEY
@@ -5,6 +25,13 @@ from homeassistant.core import callback
 from .const import DOMAIN, CONF_API_KEY, CONF_METERS, CONF_TIMEZONE, CONF_ALERT_PRICE, CONF_ALERT_USAGE
 
 class ICTPartnerDlaPstrykConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        return ICTPartnerDlaPstrykOptionsFlowHandler(config_entry)
     """Handle a config flow for ictpartnerdlapstrykpl."""
 
     VERSION = 1
